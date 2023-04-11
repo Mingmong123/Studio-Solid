a2ffb81423d5973c9758d25dcd0f96b17d7a0def
Ferdynand chandra <95560483+Mingmong123@users.noreply.github.com>
2023-04-11 10:16:28 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,13 +7,5 @@ export default function Home() {
       <Title>Halo kelas studio</Title>
       <h1>Halo kelas studio</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
   );
 }
